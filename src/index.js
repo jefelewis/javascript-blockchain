d5036f774e673250032ceadcb19c25921f1d9fa5
Jeff Lewis <jeffylew77@gmail.com>
2018-11-13 22:36:13 -0800

    [Dev] Adding Block constructor

---
+++ src/index.js
@@ -0,0 +1,11 @@
+class Block {
+  constructor(index, data, timestamp, previousHash = '') {
+    this.index = index;
+    this.data = data;
+    this.timestamp = timestamp;
+    this.previousHash = previousHash;
+  }
+
+
+
+}
